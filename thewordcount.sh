d9c28ac86d6b82662fb07e6aa56f1a810cdac7d5
0ddy <isvanderpuije@gmail.com>
2024-01-11 11:12:56 +0000

    Update thewordcount.sh

--- thewordcount.sh
+++ thewordcount.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
 
+declare -A words_freq
+for word in $(xargs -n1 < $1 | tr -d '[:punct:]' | sort -fu)
+do 
+    thecount=`grep -io "\b$word\b" $1 | wc -l`
+    words_freq[$word]=$thecount
+done
+
+the_words=${!words_freq[@]}
+for the_key in $the_words
+do
+    echo $the_key ":" ${words_freq[$the_key]}
+done | sort -rn -k3
